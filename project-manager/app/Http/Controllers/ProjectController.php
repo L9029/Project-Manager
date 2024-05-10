f8e2b8062dad7286026c54e9ad803a77d7892343
L9029 <angeloca03@gmail.com>
2024-05-10 16:10:15 -0400

    Developing the method getAllProjects that return from the model Project all the data from de DB and configuring the route that get the view where show all the data.

--- project-manager/app/Http/Controllers/ProjectController.php
+++ project-manager/app/Http/Controllers/ProjectController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    /**
+     * Return all the data from a consult to the table Projects from the DB.
+     * 
+     * @return array return an array with all the data from the table Projects.
+     */
+    public function getAllProjects(){
+        $projects = Project::all();
+
+        return $projects;
+    }
 }
